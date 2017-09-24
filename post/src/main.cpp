b31131385d91cf7fa80db2d006604f455acc5c3d
Agustin Sanchez <trorik23@gmail.com>
2017-09-23 23:16:35 -0300

    se leen args de POST

---
+++ post/src/main.cpp
@@ -0,0 +1,89 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include "HTML.h"
+#include "SSID.h"
+ 
+ESP8266WebServer server(80); //server init, port 80
+
+#define ROWS 8
+#define COLUMNS 24
+
+int matrix [ROWS][COLUMNS];
+
+void handleRoot(){
+    server.send ( 200, "text/html", INDEX_HTML);
+}
+
+void handleNotFound() {
+    //redirect to root "/"
+    server.sendHeader("Location", String("/"), true);
+    server.send ( 302, "text/plain", "");
+    //server.send ( 404, "text/plain", "ERROR 404: Tas re perdido wacho" );
+}
+
+void handleMatrix(){
+    for(int row = 0; row < ROWS; row++){
+        for(int column = 0; column < COLUMNS; column++){
+            String aux= String(row) + "-" + String(column);
+            Serial.print(aux);
+            Serial.print(" | ");
+            Serial.println(server.arg(aux));
+            matrix[row][column] = server.arg(aux)=="1"?1:0;
+        }
+    }
+    for(int row = 0; row < ROWS; row++){
+        for(int column = 0; column < COLUMNS; column++){
+            if(matrix[row][column] == 1){
+                Serial.print("X");                
+            }else{
+                Serial.print("0");
+            }
+        }
+        Serial.println("");
+    }
+    //redirect to root "/"
+    server.sendHeader("Location", String("/"), true);
+    server.send ( 302, "text/plain", "");
+}
+void handleArgs(){
+    String arg1 = server.arg("arg1");
+    String arg2 = server.arg("arg2");
+    Serial.print("Argumento 1: ");
+    Serial.println(arg1);
+    Serial.print("Argumento 2: ");
+    Serial.println(arg2);
+
+    //redirect to root "/"
+    server.sendHeader("Location", String("/"), true);
+    server.send ( 302, "text/plain", "");
+}
+
+void setup() {
+    Serial.begin(115200);
+
+    Serial.print("Connecting to ");
+    Serial.println(SSID);
+    WiFi.begin(SSID, PASSWORD);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(200);
+        Serial.print(".");
+    }
+    Serial.println("\nConnected!");
+
+    // Usually 192.168.0.26
+    Serial.print("IP: ");
+    Serial.println(WiFi.localIP());
+
+    //roots
+    server.on("/",handleRoot);
+    server.on("/args",handleArgs);
+    server.on("/matrix",handleMatrix);
+    server.onNotFound(handleNotFound);
+    server.begin(); //start webserver
+}
+ 
+ void loop (){
+    server.handleClient();
+}

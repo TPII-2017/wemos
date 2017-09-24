e9484bfd9a77fd8f5f74a76464c10837a6089056
Agustin Sanchez <trorik23@gmail.com>
2017-09-24 18:45:44 -0300

    libreria para las letras

---
+++ arduino_server/Alphabet.h
@@ -0,0 +1,12 @@
+#ifndef ALPHABET_H
+#define ALPHABET_H
+
+#define CHAR_HEIGHT 8
+#define CHAR_WIDTH 5
+#define ALPHABET_SIZE 26
+
+extern const char** uppercase_alphabet;
+extern const char** lowercase_alphabet;
+extern const char** numbers;
+
+#endif

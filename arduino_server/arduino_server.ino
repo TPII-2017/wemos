1ea96d31c921b169a0016e390ea3e25d4f33af5b
agusss.garcia19 <agusss.garcia19@gmail.com>
2017-09-24 23:29:33 -0300

    Arreglos menores a la lectura de parametros, todos los argumentos funcionando

--- arduino_server/arduino_server.ino
+++ arduino_server/arduino_server.ino
@@ -4,80 +4,41 @@
 #include "LedMatrix.h"
 #include "Authentication.h"
 #include "LedMatrix.h"
+#include "WString.h"
 
 ESP8266WebServer server(80);
 
-void handle_index() {
-	server.send(200, "text/html", FileManager::read_file("/index.html"));
-}
+String index_html_file;
+String config_html_file;
 
-void handle_authentication() {
-	if(server.arg("token") == TOKEN){
-        handle_config();
-    }else{
-        handle_index();
-    }
-}
+void go_to_config(int status);
+void go_to_index(int status, String message);
+void handle_post_authentication();
+void handleNotFound();
+void handle_post_phrase();
+void handle_post_matrix();
+void handle_post_predefined();
 
-void handle_config() {
-    server.send(200, "text/html", FileManager::read_file("/config.html"));
-}
-
-void handleNotFound() {
-    //redirect to root "/"
-    server.sendHeader("Location", String("/"), true);
-    server.send ( 302, "text/plain", "");
-}
-
-void redirect_config(){
-    //redirect to "/config.html"
-    server.sendHeader("Location", String("/config.html"), true);
-    server.send ( 302, "text/plain", ""); // redirect URI response
-}
-
-void handle_matrix(){
-    for(int row = 0; row < ROWS; row++){
-        for(int column = 0; column < COLUMNS; column++){
-            matrix[row][column] = server.arg(String(row) + "-" + String(column))=="1"?1:0;
-        }
-    }
-
-    //for debug
-    for(int row = 0; row < ROWS; row++){
-        for(int column = 0; column < COLUMNS; column++){
-            if(matrix[row][column] == 1){
-                Serial.print("X");                
-            }else{
-                Serial.print("0");
-            }
-        }
-        Serial.println("");
-    }
-    redirect_config();   
-}
-
-void handle_phrase(){
-    String phrase = server.arg("phrase");
-    Serial.print("Caracteres que se deberian mostrar: ");
-    Serial.println(phrase);
-    redirect_config();
-}
-
-void setup() {
-
+void setup()
+{
     Serial.begin(115200);
     WiFi.begin("Agustin", "doctor reformulemelo");
 
     if (!SPIFFS.begin())
         Serial.println("No se pudo abrir el file system.");
 
-	Serial.println();
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(100); Serial.print(".");
-    }
-    Serial.println();    
+    index_html_file = FileManager::read_file("/index.html");
+    config_html_file = FileManager::read_file("/config.html");
 
-    Serial.print("IP address: "); Serial.println(WiFi.localIP());
+    Serial.println();
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(100);
+        Serial.print(".");
+    }
+    Serial.println();
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
 
     server.serveStatic("/static/favicon.png", SPIFFS, "/static/favicon.png");
     server.serveStatic("/static/bootstrap.min.css", SPIFFS, "/static/bootstrap.min.css");
@@ -85,16 +46,92 @@ void setup() {
     server.serveStatic("/static/popper.min.js", SPIFFS, "/static/popper.min.js");
     server.serveStatic("/static/bootstrap.min.js", SPIFFS, "/static/bootstrap.min.js");
     server.serveStatic("/static/my-styles.css", SPIFFS, "/static/my-styles.css");
-    server.on("/config.html", handle_config);
-    server.on("/phrase", handle_phrase);
-    server.on("/matrix", handle_matrix);
+    server.serveStatic("/", SPIFFS, "/index.html");
+
     server.onNotFound(handleNotFound);
-    server.on("/", handle_index);
-    server.on("/authentication", handle_authentication);
+    server.on("/authentication", HTTP_POST, handle_post_authentication);
+    server.on("/phrase", HTTP_POST, handle_post_phrase);
+    server.on("/matrix", HTTP_POST, handle_post_matrix);
+    server.on("/predefined", HTTP_POST, handle_post_predefined);
 
     server.begin();
 }
 
-void loop() {
+void handleNotFound()
+{
+    server.sendHeader("Location", String("/"), true);
+    server.send(302, "text/plain", "");
+}
+
+void handle_post_authentication()
+{
+    if (server.arg("token").equals(TOKEN))
+    {
+        Serial.println("Acceso permitido");
+        go_to_config(200);
+    }
+    else
+    {
+        Serial.println("Acceso denegado");
+        go_to_index(200, "La contraseña ingresada es incorrecta");
+    }
+}
+
+void go_to_config(int status)
+{
+    server.send(status, "text/html", config_html_file);
+}
+
+void go_to_index(int status, String message)
+{
+    //TODO tiene que haber una manera de pasar mensajes al html. Ver server.sendContent();
+    server.send(status, "text/html", index_html_file);
+}
+
+void handle_post_phrase()
+{
+    String phrase = server.arg("phrase");
+    Serial.print("Caracteres que se deberian mostrar: ");
+    Serial.println(phrase);
+    go_to_config(302);
+}
+
+void handle_post_matrix()
+{
+    for (int row = 0; row < ROWS; row++)
+        for (int column = 0; column < COLUMNS; column++)
+            matrix[row][column] = server.arg(String(row) + "-" + String(column)) == "1" ? 1 : 0;
+
+    //for debug
+    for (int row = 0; row < ROWS; row++)
+    {
+        for (int column = 0; column < COLUMNS; column++)
+        {
+            if (matrix[row][column] == 1)
+                Serial.print("X");
+            else
+                Serial.print("0");
+        }
+        Serial.println("");
+    }
+    go_to_config(302);
+}
+
+void handle_post_predefined()
+{
+	String value = server.arg("image-predif");
+	if (value.equals("smile-face"))
+		Serial.println("Smile Face");
+	else if (value.equals("pacman"))
+		Serial.println("Pacman");
+	else if (value.equals("new-life"))
+		Serial.println("New life");
+	else
+		Serial.println("Error al elegir el predefinido");
+	go_to_config(302);
+}
+
+void loop()
+{
     server.handleClient();
 }
